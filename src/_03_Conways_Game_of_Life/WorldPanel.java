bd53b33118d606d50769eaeadd4646a54e8a99fa
tate <tate@localhost>
2021-03-11 16:58:24 -0800

    ConwaysGameOfLife, MazeMaker(in progress)

--- src/_03_Conways_Game_of_Life/WorldPanel.java
+++ src/_03_Conways_Game_of_Life/WorldPanel.java
@@ -114,7 +114,7 @@ public class WorldPanel extends JPanel implements MouseListener, ActionListener 
 
 		for (int i = 0; i < cells.length; i++) {
 			for (int j = 0; j < cells[i].length; j++) {
-				cells[i][j].liveOrDie(getLivingNeighbors(i, j));
+				cells[i][j].liveOrDie(livingNeighbors[i][j]);
 			}
 		}
 
